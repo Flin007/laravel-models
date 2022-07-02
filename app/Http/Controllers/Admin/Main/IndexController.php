f53186660f39e678f51a19fd770b44174b2ab7a9
Vladimir <v.fetisov@smartis.bi>
2022-07-03 02:49:34 +0300

    Вывод кол-ва юзеров в ПУ

--- app/Http/Controllers/Admin/Main/IndexController.php
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin\Main;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class indexController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.main.index');
+        $userCount = User::count();
+        return view('admin.main.index', compact('userCount'));
     }
 }
