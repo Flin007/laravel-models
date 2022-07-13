cf7582ed07a0acefefcc2c4d51ff112908696d0e
Vladimir <v.fetisov@smartis.bi>
2022-07-14 02:12:55 +0300

    Страница входа

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,55 @@
 @extends('layouts.app')
 @section('title', 'Вход - ORAY AGENCY')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="login_register">
+        <div class="header">
+            Авторизация
+        </div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="input_row @error('email') error @enderror">
+                <label for="email">Email</label>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" required placeholder="Ваш email...">
+                @error('email')
+                <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+            <div class="input_row  @error('password') error @enderror">
+                <label for="password">Пароль</label>
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                       name="password" placeholder="Ваш пароль...">
+                @error('password')
+                <span class="error">{{ $message }}</span>
+                @enderror
+            </div>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+            <div class="input_row">
+                <div class="remember">
+                    <input class="form-check-input" type="checkbox" name="remember"
+                           id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    <label class="form-check-label" for="remember">
+                        Запомнить меня
+                    </label>
+                </div>
+                <button type="submit" class="submit">
+                    Начать сеанс
+                </button>
+            </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+            <div class="footer_links">
+                <div class="row">
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        Забыли пароль?
+                    </a>
+                </div>
+                <div class="row">
+                    <a href="{{ route('register') }}">
+                        Еще нет аккаунта?
+                    </a>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
-</div>
 @endsection
