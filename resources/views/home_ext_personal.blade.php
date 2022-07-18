ff1e26918dce7272d155ea827896668ae60fbed1
Vladimir <v.fetisov@smartis.bi>
2022-07-18 23:50:49 +0300

    about service section

--- resources/views/home_ext_personal.blade.php
+++ resources/views/home_ext_personal.blade.php
@@ -137,4 +137,30 @@
             <span>проверенных моделей</span>
         </div>
     </section>
+    <section class="about_service d-flex">
+        <div class="left"></div>
+        <div class="right">
+            <h2>О СЕРВИСЕ</h2>
+            <div class="content">
+                <div class="item">
+                    <h3>ГОСТЯМ</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                </div>
+                <div class="item">
+                    <h3>МОДЕЛЯМ</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
